885b3af7159215b34a8a7271709418f5e9bc46b3
BenMiriello <benmiriello@gmail.com>
2021-01-04 19:20:54 -0500

    Mostly complete timer, except for not allowing parallel tasks.

--- features/setTimer.py
+++ features/setTimer.py
@@ -1,5 +1,6 @@
 import time
 import sys
+from word2number import w2n
 
 from utilities.speak import speak
 
@@ -12,21 +13,38 @@ def conditions(query):
 
   return any(string in query for string in validQueries)
 
-def response(query=None):
+def plural(integer):
+  return 's' if integer > 1 else ''
+
+def response(query):
+  query_array = query.split()
+
+  def query_time(increment):
+    index = 1
+    if increment in query_array:
+      index = query_array.index(increment)
+    elif increment + 's' in query_array:
+      index = query_array.index(increment + 's')
+    else:
+      return 0
+
+    try:
+      return w2n.word_to_num(query_array[index - 1])
+    except:
+      return 0
+
+  seconds = query_time('second')
+  minutes = query_time('minute')
+  hours = query_time('hour')
+
+  seconds_now = minutes_now = hours_now = 0
+
   time_start = time.time()
 
-  end_hours = 0
-  end_minutes = 0
-  end_seconds = 10
-
-  seconds = 0
-  minutes = 0
-  hours = 0
-
   initial_report = "\n\rSetting timer for {hours}{minutes}{seconds}".format(
-    hours = str(end_hours) + ' hours ' if (end_hours > 0) else '',
-    minutes = str(end_minutes) + ' minutes ' if (end_minutes > 0) else '',
-    seconds = str(end_seconds) + ' seconds ' if (end_seconds > 0) else ''
+    hours = str(hours) + ' hours ' if (hours > 0) else '',
+    minutes = str(minutes) + ' minutes ' if (minutes > 0) else '',
+    seconds = str(seconds) + ' seconds ' if (seconds > 0) else ''
   )
   print(initial_report)
   speak(initial_report)
@@ -34,26 +52,26 @@ def response(query=None):
   while True:
     try:
       print("\r{hours}{minutes}{seconds}".format(
-        hours = str(hours) + ' hours ' if (hours > 0) else '',
-        minutes = str(minutes) + ' minutes ' if (minutes > 0) else '',
-        seconds = str(seconds) + ' seconds ' if (seconds > 0) else ''
+        hours = str(hours_now) + ' hour' + plural(hours_now) + ' ' if (hours_now > 0) else '',
+        minutes = str(minutes_now) + ' minute' + plural(minutes_now) + ' ' if (minutes_now > 0) else '',
+        seconds = str(seconds_now) + ' second' + plural(seconds_now) + ' ' if (seconds_now > 0) else ''
       ))
 
       time.sleep(1)
-      seconds = int(time.time() - time_start) - minutes * 60
+      seconds_now = int(time.time() - time_start) - minutes_now * 60
 
-      if seconds >= 60:
-        minutes += 1
-        seconds = 0
+      if seconds_now >= 60:
+        minutes_now += 1
+        seconds_now = 0
 
-      if minutes >= 60:
-        hours += 1
-        minutes = 0
-        seconds = 0
+      if minutes_now >= 60:
+        hours_now += 1
+        minutes_now = 0
+        seconds_now = 0
 
-      if end_hours <= hours:
-        if end_minutes <= minutes:
-          if end_seconds <= seconds:
+      if hours <= hours_now:
+        if minutes <= minutes_now:
+          if seconds <= seconds_now:
             final_report = "\n\rTimer is up\n"
             print(final_report, end=' ', flush=True)
             speak(final_report)
