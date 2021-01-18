02aa6d227d49a143c2ea49bf25b9f22a6186befa
Ben Miriello <benmiriello@gmail.com>
2021-01-18 10:32:09 -0500

    Add device_index and if name main line to takeCommand()

--- utilities/takeCommand.py
+++ utilities/takeCommand.py
@@ -5,7 +5,7 @@ def takeCommand():
 
   r = sr.Recognizer()
 
-  with sr.Microphone() as source:
+  with sr.Microphone(device_index=0) as source:
     print('Listening')
 
     r.pause_threshold = 0.5
@@ -21,3 +21,6 @@ def takeCommand():
       print(e)
       print("Say that again")
       return "None"
+
+if __name__ == '__main__':
+  takeCommand()
